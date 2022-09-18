eac5fc9156cfcdfa617cc1c983d375f018eba282
Pauline Ondiek <paulineondiek76@gmail.com>
2022-09-18 06:23:35 -0400

    created the ContactUs component and the Contact page

---
+++ src/Pages/Contact.js
@@ -0,0 +1,12 @@
+import Nav from "../components/Nav"
+import ContactUs from "../components/ContactUs"
+
+const Contact=()=>{
+    return(
+        <div>
+            <ContactUs></ContactUs>
+
+        </div>
+    )
+}
+export default Contact 
---
+++ src/components/ContactUs.js
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+const Container=styled.div``
+const 
+
+const contactUs=()=>{
+    return(
+        <ContactContainer>
+            <Container>
+                <ContactInput>
+                <input type="text"></input>
+                <input type="text"></input>
+                <input type="text"></input>
+                <input type="text"></input>
+                </ContactInput>
+            </Container>
+        </ContactContainer>
+    )
+}
